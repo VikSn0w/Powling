e9e0085d74a5a73d5b0b7a0edfc7d95653717169
viksn0w <vittorio.picone001@studenti.uniparthenope.it>
2023-03-30 00:00:51 +0200

    starter

--- Powling/ContentView.swift
+++ Powling/ContentView.swift
@@ -7,82 +7,186 @@
 
 import SwiftUI
 import CoreData
+import UIKit
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+   
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ZStack(alignment: .center){
+            Background()
+            VStack(spacing: 150){
+                Spacer()
+                MainTitle()
+            
+                ZStack (alignment: .bottom){
+                    VStack(spacing: 50) {
+                        
+                        Mode_1()
+                        Mode_2()
+                        
+                        VStack {
+                            
+                            SpacerPrinter(howManySpacers: 6)
+                            
+                            HStack() {
+                                
+                                SpacerPrinter(howManySpacers: 2)
+                                
+                                Label_1()
+                                
+                                SpacerPrinter(howManySpacers: 2)
+
+                                Settings()
+                                
+                                SpacerPrinter(howManySpacers: 2)
+
+                            }
+                            Spacer()
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+    }
+}
+
+public extension View {
+    func fullBackground(imageName: String) -> some View {
+       return background(
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+       )
+    }
+}
+
+struct StrokeText: View {
+    let text: String
+    let width: CGFloat
+    let color: Color
+
+    var body: some View {
+        ZStack{
+            ZStack{
+                Text(text).offset(x:  width, y:  width)
+                Text(text).offset(x: -width, y: -width)
+                Text(text).offset(x: -width, y:  width)
+                Text(text).offset(x:  width, y: -width)
+            }
+            .foregroundColor(color)
+            Text(text)
+        }
+    }
+}
+
+struct Background: View {
+    var body: some View {
+        Image("Background_Title")
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.all)
+            .frame(width: UIScreen.main.bounds.width+20, height: UIScreen.main.bounds.height)
+    }
+}
+
+struct MainTitle: View {
+    var body: some View {
+        StrokeText(text: "POWLING", width: 3, color: .red)
+            .foregroundColor(.white)
+            .font(
+                .system(
+                    size: 55,
+                    weight: .heavy,
+                    design: .rounded
+                )
+            )
+            .shadow(radius: 3)
+    }
+}
+
+struct Mode_1: View {
+    var body: some View {
+        Button(action: {}){
+            StrokeText(text: "TRAINING MODE", width: 1.5, color: .red)
+                .foregroundColor(.white)
+                .font(
+                    .system(
+                        size: 30,
+                        weight: .semibold,
+                        design: .rounded
+                    )
+                )
+            .shadow(radius: 3)
+        }
+    }
+}
+
+struct Mode_2: View {
+    var body: some View {
+        Button(action: {}){
+            StrokeText(text: "MULTIPLAYER", width: 1.5, color: .red)
+                .foregroundColor(.white)
+                .font(
+                    .system(
+                        size: 30,
+                        weight: .semibold,
+                        design: .rounded
+                    )
+                ).shadow(radius: 3)
+        }
+    }
+}
+
+
+struct SpacerPrinter: View {
+    public var howManySpacers: Int = 0
+    var body: some View {
+        ForEach(0...self.howManySpacers, id: \.self){_ in
+            Spacer()
+
+        }
+    }
+}
+
+
+struct Settings: View {
+    var body: some View {
+        Button(action: {}){
+            ZStack{
+                Image(systemName: "gearshape.fill")
+                    .symbolRenderingMode(.hierarchical)
+                    .foregroundColor(.white)
+                    .font(.system(size: 30))
+                    .shadow(radius: 3)
+                Image(systemName: "gearshape")
+                    .symbolRenderingMode(.hierarchical)
+                    .foregroundColor(.red)
+                    .font(.system(size: 30))
+            }
+        }
+    }
+}
+
+struct Label_1: View {
+    var body: some View {
+        Button(action: {}){
+            StrokeText(text: "RULES", width: 1.5, color: .red)
+                .foregroundColor(.white)
+                .font(
+                    .system(
+                        size: 20,
+                        weight: .semibold,
+                        design: .rounded
+                    )
+                ).shadow(radius: 3)
+        }
     }
 }
